cf755acded280f9c74eafee49bda6afe3a78017f
LeeSeongHa <toywwy@gmail.com>
2016-10-17 05:40:22 +0900

    OPENGL day5 Light

---
+++ Day05/d5_ex1_pot.cpp
@@ -0,0 +1,156 @@
+#include<iostream>
+#include<WIndows.h> //이건 GUI 때문에 필요한건데.
+#include<gl/glut.h> //이건 유틸리티 툴킷인데, 사용자의 입력이나 화면 윈도우를 제어하기 위해서다.
+#include <math.h> //삼각함수 쓰려고
+
+/*
+* gl.h (OpenGL Core Library) - 렌더링 기능을 제공하는 라이브러리
+* glu.h (OpenGL Utility Library) - GL 라이브러러의 도우미 역할
+* glut.h (OpenGL Utility Toolkit) - 사용자 입력을 받아들이거나 화면 윈도우를 제어하기 위한 라이브러리
+*/
+
+using namespace std;
+
+GLUquadricObj *quadricObj;//곡면 그릴떄 사용하는 것으로 유추됨 원통 실린더 그릴때 사용
+//곡면 그릴때 부드럽게 그리려고 사용을 하는 것 같다.
+
+static float angle = 5.0f;
+
+bool baseToggle[2];
+bool lowerToggle[2];
+bool upperToggle[2];
+
+static float theta[3];
+
+//콜백방식으로 작동한다. unsigned char 로 키를 입력받음
+void keyboard(unsigned char c, int x, int y)
+{
+	switch (c)
+	{
+	case VK_ESCAPE:
+		gluDeleteQuadric(quadricObj);
+		exit(0);
+		break;
+	}
+}
+
+//Special은 몇개 키가 없늗네도 ,,, int로 받네 이유를 모르겠다... char는이미 다른키가 매핑
+//되어있다고 가정을 해서 그런가???
+void specialKeyboard(int c, int x, int y)
+{
+	switch (c)
+	{
+	case GLUT_KEY_HOME:
+		upperToggle[0] = true;
+		upperToggle[1] = true;
+		break;
+	case GLUT_KEY_END:
+		upperToggle[0] = true;
+		upperToggle[1] = false;
+		break;
+	case GLUT_KEY_LEFT:
+		baseToggle[0] = true;
+		baseToggle[1] = true;
+
+		break;
+	case GLUT_KEY_RIGHT:
+		baseToggle[0] = true;
+		baseToggle[1] = false;
+		break;
+	case GLUT_KEY_UP:
+		lowerToggle[0] = true;
+		lowerToggle[1] = true;
+		break;
+	case GLUT_KEY_DOWN:
+		lowerToggle[0] = true;
+		lowerToggle[1] = false;
+		break;
+	}
+
+}
+
+void timer(int value)
+{
+	glutPostRedisplay();//화면 갱신이 필요할 때 이함수를 부르면 display 콜백 함수를 다시 한 번 실행
+	glutTimerFunc(1000 / 30, timer, 1);//다이머 시간 마다 갱신,  해준다 //30 frame 나온다.
+	//타이머가 작동하는데 다시 타이머를 동작시킴으로써 계쏙 작동하게 되는거지.
+}
+
+void RenderScene(void)
+{
+	//Clear the window with current clearing color
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //버퍼의 조합을 지운다.
+														/*  DEPTH_BUFFER를 두어서 픽셀별로 깊이 값을 저장하도록 하였다. 이를 이용해서 어떤 물체를 그리고자 할때 그앞에 물체가 있으면 있을때 깊이를
+														비교하여 DEPTH가 작은 삼각형쪽은 그려지고, DEPTH가 큰 사각형쪽은 그리지 않게 할 수 있게되는 것이다.
+														(동일한 좌표의 픽셀이 있을때 깊이에 따라 그려야 할지 말아야 할지 선택하게 되는 것이다.) */
+
+	glMatrixMode(GL_MODELVIEW);//MODELVEIW는 그리는거고 projection은 투영하는거다.
+	glLoadIdentity();
+
+	gluLookAt(0, 0, 2.0, 0, 0, 0, 0, 1.0, 0); //보는 시점이다.
+	//처음이 눈, at은 어느부분을 볼것인가 이기때문에 거의 안건들여..
+	//up은 축을 말하는거다...어느축으로 볼것인가.
+
+	glRotatef(theta[2], 0, 0, 1);
+	glRotatef(theta[1], 0, 1, 0);
+	glRotatef(theta[0], 1, 0, 0);
+
+	glColor3f(1, 0, 0);//RED 사용하겠다.
+
+	glutWireTeapot(1.0);//추가된 부분!
+
+	glutSwapBuffers(); //GLUT_DOUBLE 버퍼를 더블로해서 스왑해서사용할 것이다.
+}
+
+void SetupRC(void) {
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glEnable(GL_DEPTH_TEST);
+}
+
+void init(void)
+{
+
+	quadricObj = gluNewQuadric();
+	gluQuadricDrawStyle(quadricObj, GLU_LINE);
+	glutTimerFunc(1000 / 30, timer, 1);
+}
+
+void ChangeSize(int w, int h)
+{
+	if (h == 0)// 0으로 나누어지는거 막기위해서
+		h = 1;
+
+	glViewport(0, 0, w, h); // 실제로 보여지는 부분을 뷰포트라 명시한다.
+
+	glMatrixMode(GL_PROJECTION); //이부분이 PROJECTION인 이유는 3D를 2D로 투영시켜야한다.
+	//glOrtho를 통해서
+	glLoadIdentity();//좌표계 초기화
+	//근데 이게 보면 아는데 ,이러고 나서 좌표계를 초기화해줘야 하고 이런게 기억안날텐데...
+	//사실근데 안써도 상관없는거구나 ....좌표계를 조작하지 않은이상????
+	//여기에서 초기화 해주는 이유는 뭘까?? 어떤 좌표계를 초기화 시켜준다는 말이지??
+
+	if (w <= h)
+		glOrtho(-2.0, 2.0, -2.0*(float)h / (float)w,
+			2.0*(float)h / (float)w, -10.0, 10.0);
+	else
+		glOrtho(-2.0*(float)w / (float)h, 2.0*(float)w / (float)h,
+			-2.0, 2.0, -10.0, 10.0);
+}
+
+
+void main(int argc, char * argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(600, 600);
+	glutCreateWindow("Simple");
+	glutDisplayFunc(RenderScene);//렌더링되는 부분
+	glutReshapeFunc(ChangeSize);//SIZE가 바뀔마다 호출이 된다.
+//	glutSpecialFunc(specialKeyboard);//스페셜 키보드 이벤트를 콜백
+//	glutKeyboardFunc(keyboard);//키보드 이벤트 콜백
+
+	init();
+	SetupRC();
+	glutMainLoop();
+	//기본적인 틀같은거 외우필요없고 시험에안나온다 시험볼떄 준다.
+}
---
+++ Day05/d5_ex2_lighting.cpp
@@ -0,0 +1,157 @@
+#include<iostream>
+#include<WIndows.h>
+//#include<gl/gL.h>
+//#include<gl/gLU.h> //그래서 앞에를 대문자로 써도 소문자로 써도 상관이 없던건가???
+#include<gl/glut.h>//잉 이것만 있으면 되는건가?
+
+#include <math.h>
+
+using namespace std;
+
+GLUquadricObj *quadricObj;//곡면 그릴떄 사용하는 것으로 유추됨 원통 실린더 그릴때 사용
+
+static float angle = 5.0f;
+
+bool baseToggle[2];
+bool lowerToggle[2];
+bool upperToggle[2];
+
+static float theta[3];
+
+void keyboard(unsigned char c, int x, int y)
+{
+	switch (c)
+	{
+	case VK_ESCAPE:
+		gluDeleteQuadric(quadricObj);
+		exit(0);
+		break;
+	}
+}
+
+void specialKeyboard(int c, int x, int y)
+{
+	switch (c)
+	{
+	case GLUT_KEY_HOME:
+		upperToggle[0] = true;
+		upperToggle[1] = true;
+		break;
+	case GLUT_KEY_END:
+		upperToggle[0] = true;
+		upperToggle[1] = false;
+		break;
+	case GLUT_KEY_LEFT:
+		baseToggle[0] = true;
+		baseToggle[1] = true;
+
+		break;
+	case GLUT_KEY_RIGHT:
+		baseToggle[0] = true;
+		baseToggle[1] = false;
+		break;
+	case GLUT_KEY_UP:
+		lowerToggle[0] = true;
+		lowerToggle[1] = true;
+		break;
+	case GLUT_KEY_DOWN:
+		lowerToggle[0] = true;
+		lowerToggle[1] = false;
+		break;
+	}
+
+}
+
+void timer(int value)
+{
+	glutPostRedisplay();//화면 갱신이 필요할 때 이함수를 부르면 display 콜백 함수를 다시 한 번 실행
+	glutTimerFunc(1000 / 30, timer, 1);//다이머 시간 마다 갱신,  해준다 //30 frame 나온다.
+}
+
+void RenderScene(void)
+{
+	//Clear the window with current clearing color
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //버퍼의 조합을 지운다.
+														/*  DEPTH_BUFFER를 두어서 픽셀별로 깊이 값을 저장하도록 하였다. 이를 이용해서 어떤 물체를 그리고자 할때 그앞에 물체가 있으면 있을때 깊이를
+														비교하여 DEPTH가 작은 삼각형쪽은 그려지고, DEPTH가 큰 사각형쪽은 그리지 않게 할 수 있게되는 것이다.
+														(동일한 좌표의 픽셀이 있을때 깊이에 따라 그려야 할지 말아야 할지 선택하게 되는 것이다.) */
+
+	glMatrixMode(GL_MODELVIEW);//MODELVEIW는 그리는거고 projection은 투영하는거다.
+	glLoadIdentity();
+
+	gluLookAt(0, 0, 2.0, 0, 0, 0, 0, 1.0, 0);// 정면에서보려고
+	//gluLookAt(0, 0, 2.0, 0, 0, 0, 0, 1.0, 0);// 정면에서보려고
+	//gluLookAt(1, 1, 2.0, 0, 0, 0, 0, 1.0, 0);// 약간위에서 본다.
+
+
+	/*glRotatef(theta[2], 0, 0, 1);
+	glRotatef(theta[1], 0, 1, 0);
+	glRotatef(theta[0], 1, 0, 0);*/
+
+
+	glEnable(GL_LIGHTING);//빛을 사용할 것다.
+	glEnable(GL_LIGHT0);//0 ㅅ용
+
+	
+	float f = 2.0f;
+
+	GLfloat light_pos[4] = { 2.0f,2.0f,2.0f,1.0f };
+	glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
+
+	//float 값 타입의 vector라고 명시하는 것이다.
+
+	//glColor3f(1, 0, 0); //어차피 해도 안먹힌다. 광원을 사용하면 glColor이 상관없다고한다.
+	//회색으로 표현이된다.
+
+	glutSolidTeapot(1.0);
+
+	glutSwapBuffers(); //GLUT_DOUBLE 버퍼를 더블로해서 스왑해서사용할 것이다.
+}
+
+void SetupRC(void) {
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glEnable(GL_DEPTH_TEST);
+}
+
+void init(void)
+{
+
+	quadricObj = gluNewQuadric();
+	gluQuadricDrawStyle(quadricObj, GLU_LINE);
+	glutTimerFunc(1000 / 30, timer, 1);
+}
+
+void ChangeSize(int w, int h)
+{
+	if (h == 0)// 0으로 나누어지는거 막기위해서
+		h = 1;
+
+	glViewport(0, 0, w, h);
+
+	glMatrixMode(GL_PROJECTION); //이부분이 PROJECTION인 이유는
+	glLoadIdentity();//좌표계 초기화
+
+	if (w <= h)
+		glOrtho(-2.0, 2.0, -2.0*(float)h / (float)w,
+			2.0*(float)h / (float)w, -10.0, 10.0);
+	else
+		glOrtho(-2.0*(float)w / (float)h, 2.0*(float)w / (float)h,
+			-2.0, 2.0, -10.0, 10.0);
+}
+
+
+void main(int argc, char * argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(600, 600);
+	glutCreateWindow("Simple");
+	glutDisplayFunc(RenderScene);//렌더링되는 부분
+	glutReshapeFunc(ChangeSize);//SIZE가 바뀔마다 호출이 된다.
+								//	glutSpecialFunc(specialKeyboard);//스페셜 키보드 이벤트를 콜백
+								//	glutKeyboardFunc(keyboard);//키보드 이벤트 콜백
+	init();
+	SetupRC();
+	glutMainLoop();
+	//기본적인 틀같은거 외우필요없고 시험에안나온다 시험볼떄 준다.
+}
